f61c03f5f2a7fe503777fe396d5eabbfc25e2f2b
Natallia Malei <natalliamalei@gmail.com>
2026-06-27 11:59:40 +0300

    feat: сохранение выбора языка в localStorage и установка EN по умолчанию

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 import { Header } from "./components/shell/Header";
 import { Footer } from "./components/shell/Footer";
 
 export default function App() {
+  const { i18n } = useTranslation();
+
+  useEffect(() => {
+    document.documentElement.lang = i18n.language;
+  }, [i18n.language]);
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
